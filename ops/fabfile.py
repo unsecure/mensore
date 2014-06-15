c6c2cb8716122664599f1e8a05220c8917dc67c0
k0c1b <kaoru@b0.local>
2014-06-15 11:07:53 +0900

    最初にチェックする内容を追加

--- ops/fabfile.py
+++ ops/fabfile.py
@@ -11,3 +11,20 @@ def check_hostname():
 
 def check_disk():
 	run("df -H")
+
+def check_mem():
+	run("free -m")
+
+def check_uptime():
+	run("uptime")
+
+def check_login():
+	run("w")
+
+def first_check():
+	check_hostname()
+	check_date()
+	check_disk()
+	check_mem()
+	check_uptime()
+	check_login()
