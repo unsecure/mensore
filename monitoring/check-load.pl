e3e74a4b2625e3b33b08a2af4192dbf8029e08c3
k0c1b <KaoruToda@users.noreply.github.com>
2014-06-15 14:30:56 +0900

    ロードアベレージの監視

---
+++ monitoring/check-load.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w -I./lib/
+
+my ($limit1, $limit5, $limit15) = split(/\s+/, `cat load.txt`);
+
+unless( `uptime` =~ /load average(?:s)?: (.+)/ ){
+	die "can not get load average by uptime";
+}
+
+my ($la1, $la5, $la15) = split(/,\s*/, $1);
+
+if( $la1 > $limit1 ){
+	print "load average 1 min: $la1 > $limit1\n";
+}
+
+if( $la5 > $limit5 ){
+	print "load average 5 min: $la5 > $limit5\n";
+}
+
+if( $la15 > $limit15 ){
+	print "load average 15 min: $la15 > $limit15\n";
+}
