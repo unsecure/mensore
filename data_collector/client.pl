b7d789cb5ea804a8c9db5a6567518a9ece3b0d61
k0c1b <kaoru@b0.local>
2014-06-15 15:37:17 +0900

    データ収集サーバー＆クライアント

---
+++ data_collector/client.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w -I./lib/
+
+use strict;
+use warnings;
+use Socket;
+
+my $tag = shift;
+unless( $tag ){
+	die "missing tag";
+}
+
+# サーバーと接続
+my $sock;
+socket($sock, PF_INET, SOCK_STREAM, getprotobyname('tcp'))
+	or die "Cannot create socket: $!";
+
+my $remote_host = 'localhost';
+my $packed_remote_host = inet_aton($remote_host)
+	or die "Cannot pack $remote_host: $!";
+
+my $remote_port = 7777;
+my $sock_addr = sockaddr_in($remote_port, $packed_remote_host)
+	or die "Cannot pack $remote_host:$remote_port: $!";
+
+connect($sock, $sock_addr)
+	or die "Cannot connect $remote_host:$remote_port: $!";
+
+my $old_handle = select $sock;
+$| = 1; 
+select $old_handle;
+
+# ヘッダ
+print $sock "$tag\n";
+
+while (defined(my $line = <STDIN>)){
+	print $sock $line;
+}
+
+# サーバーと切断
+shutdown $sock, 1;
+close $sock;
