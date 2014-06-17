f2291f0ae1e9e4a7c6bcf2d38aba118dec9e49c6
nobu <KaoruToda@users.noreply.github.com>
2014-06-17 07:40:14 -0700

    atとcronの取得スクリプト(オプションでファイル出力)

---
+++ cron/cron.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+package sub_cron;
+
+use strict;
+use warnings;
+use utf8;
+
+sub cron{
+    my $passwd = `cut -d: -f1 /etc/passwd`;
+    my @user_list=split("\n",$passwd);
+    my @result;
+    foreach my $user(@user_list) {
+        my $crontab = `crontab -u $user -l 2>/dev/null`;
+        my @find=split("\n",$crontab);
+        my $find = @find;
+        if($find eq 0){
+            next;
+        }else{
+            my @ans=split("\n",$crontab);
+            push @result,">".$user;
+            push @result,@ans; 
+        }
+    }
+    return @result;
+}
+
+sub analyze_cron{
+    my @cons = @_;
+    my @ans;
+    foreach my $finder (@cons){
+        if($finder=~m/^\s*,*$/){
+            next;
+        }
+        elsif($finder=~m/^#.+$/){
+            next;
+        }
+        push @ans,$finder;
+    }
+    return @ans;
+}
+1;
