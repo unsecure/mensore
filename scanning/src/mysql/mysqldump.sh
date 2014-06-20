04810e34e3756f763c8231b869391dc52a4f660c
centralvillages <KaoruToda@users.noreply.github.com>
2014-06-21 07:26:55 +0900

    remane backup file

--- scanning/src/mysql/mysqldump.sh
+++ scanning/src/mysql/mysqldump.sh
@@ -4,10 +4,12 @@
 #
 # must 'sudo' 
 
+DATE=$(date +%Y%m%d)
+
 if [ $# -eq 0 ]; then 
-    TARGET_FILE=.
+    TARGET_FILE=mysqldump-${DATE}
 else    
     TARGET_FILE="$@"
 fi
 
-mysqldump --single-transaction --all-databases > $TARGET_FILE
+mysqldump --single-transaction --all-databases > ~/backup-mysql/${TARGET_FILE}.sql
